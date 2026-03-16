cf1952ec8d0a43f79ad0db6a0b1cc9f754a453af
xAartT <arturbresolin20@gmail.com>
2026-03-16 19:42:03 -0300

    fix: remove env

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,13 +1,9 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+export const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASS,
   port: process.env.POSTGRES_PORT,
 });
-
-module.exports = {
-  query: (sql, params) => pool.query(sql, params)
-};
