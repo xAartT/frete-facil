8f99130c76f4b4960fba6131d4f48d8687e6be7d
gustavohubner-horus <gustavo.hubner@horus.edu.br>
2026-03-30 21:55:01 -0300

    Primeiras mudanças na pagina inicial

---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      bordered
+      :mini="miniState"
+      :width="250"
+      :breakpoint="500"
+      style="background-color: #F8FAFC;"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          
+          <q-item>
+            <q-item-section avatar>
+              <q-btn flat dense round icon="local_shipping" aria-label="Menu" @click="toggleMiniState" />
+            </q-item-section>
+          </q-item>
+
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+          />
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+
+
+
+const essentialLinks = [
+  { title: 'Dashboard', icon: 'dashboard', link: 'https://quasar.dev' },
+  { title: 'Solicitar frete', icon: 'add_box', link: 'https://github.com/quasarframework' },
+  { title: 'Oferece frete', icon: 'sell', link: 'https://chat.quasar.dev' },
+  { title: 'Meus fretes', icon: 'record_voice_over', link: 'https://forum.quasar.dev' },
+  { title: 'Mensagens', icon: 'rss_feed', link: 'https://twitter.com/quasarframework' }
+]
+
+const drawer = ref(true)
+const miniState = ref(false)
+
+function toggleMiniState () {
+  miniState.value = !miniState.value
+}
+</script>
---
+++ app/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center" style="background-color: #F8F9FA;">
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage'
+});
+</script>
