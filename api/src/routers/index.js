df147d4af18eb91aea87c5458fa29664541b8dcf
Joao <theblackgamer1254@gmail.com>
2026-06-08 19:49:08 -0300

    crud veiculos finalizada

--- api/src/routers/index.js
+++ api/src/routers/index.js
@@ -3,6 +3,7 @@ import authRouter from './authRouter.js';
 import usuariosRouter from './usuariosRouter.js';
 import veiculosRouter from './veiculosRouter.js';
 import enderecosRouter from './enderecosRouter.js';
+import { autenticar } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
@@ -10,7 +11,7 @@ router.use('/auth', authRouter);
 router.use('/usuarios', usuariosRouter);
 
 router.use('/veiculos',   veiculosRouter);
-router.use('/enderecos',  enderecosRouter);
+router.use('/enderecos', autenticar, enderecosRouter);
 // router.use('/encomendas', encomendasRouter);
 // router.use('/propostas',  propostasRouter);
 // router.use('/pagamentos', pagamentosRouter);
